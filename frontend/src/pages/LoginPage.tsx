8553d8edfaf43c7361797d9f233e9ceee3f0e1e8
Daniel <danielfeng17@gmail.com>
2024-10-22 13:26:39 -0400

    Initial Sample MERN files

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,16 @@
+import PageTitle from '../components/PageTitle.tsx';
+import Login from '../components/Login.tsx';
+
+const LoginPage = () =>
+{
+
+    return(
+      <div>
+        <img src="./src/assets/cat.gif"></img>
+        <PageTitle />
+        <Login />
+      </div>
+    );
+};
+
+export default LoginPage;
