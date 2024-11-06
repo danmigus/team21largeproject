94dd50bafea14f7a0caa1f6674761849f9d22a41
Whiskk <150847662+Whiskk@users.noreply.github.com>
2024-11-06 01:02:11 -0500

    Tried to implement the formula

--- frontend/src/components/Analyze.tsx
+++ frontend/src/components/Analyze.tsx
@@ -78,8 +78,8 @@ function Analyze()
         console.log(card);
         setPlayerCards([...playerCard, card]);
 
-        console.log("Adding: " + JSON.parse(card).rank_ecr);
-        let newEcr:Number = JSON.parse(card).rank_ecr + Number(ecr);
+        console.log("Adding: " + (15 - ((JSON.parse(card).rank_ecr - 1) * 0.05)));
+        let newEcr:Number = (15 - ((JSON.parse(card).rank_ecr - 1) * 0.05)) + Number(ecr);
         handleEcr(newEcr.toString());
     }
 
