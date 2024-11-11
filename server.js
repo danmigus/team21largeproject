84adf03f01bc35c1340b3a02bcd81411b89005d6
Whiskk <150847662+Whiskk@users.noreply.github.com>
2024-11-11 00:04:39 -0500

    Got rid of the card stuff

--- server.js
+++ server.js
@@ -63,30 +63,6 @@ async function sendEmail (req, res, next) {
 }
 
 
-app.post('/api/addcard', async (req, res, next) =>
-{
-  // incoming: userId, color
-  // outgoing: error
-  
-  const { userId, card } = req.body;
-
-  const newCard = {Card:card,UserId:userId};
-  var error = '';
-
-  try
-  {
-    const db = client.db();
-    const result = db.collection('Cards').insertOne(newCard);
-  }
-  catch(e)
-  {
-    error = e.toString();
-  }
-
-  var ret = { error: error };
-  res.status(200).json(ret);
-});
-
 app.post('/api/newroster', async (req, res, next) =>{
 
   //incoming: userId, rosterName
@@ -571,31 +547,5 @@ app.post('/api/addplayers', async (req, res) =>
 
 });
 
-
-  
-app.post('/api/searchcards', async (req, res, next) => 
-  {
-    // incoming: userId, search
-    // outgoing: results[], error
-  
-    var error = '';
-  
-    const { userId, search } = req.body;
-  
-    var _search = search.trim();
-    
-    const db = client.db();
-    const results = await db.collection('Cards').find({Card:{$regex:_search+'.*', $options:'i'}, UserId:userId}).toArray();
-    
-    var _ret = [];
-    for( var i=0; i<results.length; i++ )
-    {
-      _ret.push( results[i].Card );
-    }
-    
-    var ret = {results:_ret, error:error};
-    res.status(200).json(ret);
-  });
-  
   
 
