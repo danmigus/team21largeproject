5ddc3c388c553494c7bc65d2098de2eb25b5f855
Daniel <danielfeng17@gmail.com>
2024-11-13 20:04:05 -0500

    Changed player_team filter to be a dropdown box, split into AFC and NFC teams

--- frontend/src/components/Analyze.tsx
+++ frontend/src/components/Analyze.tsx
@@ -11,6 +11,20 @@ function Analyze()
 
     const userData:any = localStorage.getItem("user_data")
     const userId:string = JSON.parse(userData).id;
+    
+    const nfc = [
+                "/--NFC--", "FA/Free Agents", "ARI/Arizona Cardinals", "ATL/Atlanta Falcons", "CAR/Carolina Panthers", "CHI/Chicago Bears",
+                "DAL/Dallas Cowboys", "DET/Detroit Lions", "GB/Green Bay Packers", "LAR/Los Angeles Rams",
+                "MIN/Minnesota Vikings", "NO/New Orleans Saints", "NYG/New York Giants", "PHI/Philly Eagles",
+                "SF/San Fran 49ers", "SEA/Seattle Seahawks", "TB/Tampa Bay Buccs", "WAS/Washington Commanders"
+                ]
+
+    const afc = [
+                "/--AFC--", "BAL/Baltimore Ravens", "BUF/Buffalo Bills", "CIN/Cincinnati Bengals", "CLE/Cleveland Browns",
+                "DEN/Denver Broncos", "HOU/Houston Texans", "IND/Indianapolis Colts", "JAC/Jacksonville Jaguars",
+                "KC/Kansas City Chiefs", "LV/Las Vegas Raiders", "LAC/Los Angeles Chargers", "MIA/Miami Dolphins",
+                "NE/New England Patriots", "NYJ/New York Jets", "PIT/Pittsburgh Steelers", "TEN/Tennessee Titans"
+                ]
 
     const [rosters, setRosters] = useState<any[]>([
         {
@@ -196,6 +210,9 @@ function Analyze()
         let revealResults = document.getElementById("searchResults") as HTMLDivElement;
         revealResults.style.display = "block";
 
+        document.getElementById('nfc')!.removeAttribute('disabled');
+        document.getElementById('afc')!.removeAttribute('disabled');
+
         let obj = {playerName, position, team};
         let js = JSON.stringify(obj);
 
@@ -264,6 +281,19 @@ function Analyze()
                 <div>
                     <input type="text" id="searchPlayers" placeholder="Enter player name" onChange={handleSearchText} />
                 </div>
+
+                <select id="nfc" className="selectTeam" onChange={handleSearchTeam} onClick={(e) => e.currentTarget.nextElementSibling!.setAttribute('disabled', 'true')}>
+                    {nfc.map((team) => (
+                        <option value={team.split('/')[0]}> {team.split('/')[1]} </option>
+                    ))}
+                </select>
+
+                <select id="afc" className="selectTeam" onChange={handleSearchTeam} onClick={(e) => e.currentTarget.previousElementSibling!.setAttribute('disabled', 'true')}>
+                    {afc.map((team) => (
+                        <option value={team.split('/')[0]}> {team.split('/')[1]} </option>
+                    ))}
+                </select>
+
                 <select className="selectPosition" onChange={handleSearchPosition}>
                     <option value="">--Position--</option>
                     <option>QB</option>
@@ -271,7 +301,7 @@ function Analyze()
                     <option>RB</option>
                     <option>TE</option>
                 </select>
-                <input type="text" className="searchTeam" placeholder="Team" onChange={handleSearchTeam} />
+
                 <div>
                     <input type="submit" id="searchButton" className="buttons" value = "Submit" onClick={searchPlayers}/>
                 </div>
