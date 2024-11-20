be6426bd24ccd1831f607f3d989b519bc6cd4a8e
Dofeno <ch284300@ucf.edu>
2024-11-20 12:25:14 -0500

    Update select_roster_dialog.dart

--- frontend/mobile/lib/select_roster_dialog.dart
+++ frontend/mobile/lib/select_roster_dialog.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
 
 class SelectRosterDialog extends StatelessWidget {
   final List<Map<String, dynamic>> rosters;
